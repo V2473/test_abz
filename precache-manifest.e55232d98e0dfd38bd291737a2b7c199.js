3dca397d3988cdc2e36c5b2868a2e551b4249200
V2473 <vovasef@gmail.com>
2020-06-25 07:23:29 +0300

    Initialize gh-pages

---
+++ precache-manifest.e55232d98e0dfd38bd291737a2b7c199.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e68d790925e1fc45351f9256ae9525",
+    "url": "./index.html"
+  },
+  {
+    "revision": "90ec93e593f0a5d6ff53",
+    "url": "./static/js/2.4dbb8415.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4dbb8415.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e1311f180c3544e87ae",
+    "url": "./static/js/main.2df5d4ad.chunk.js"
+  },
+  {
+    "revision": "4e0eef9267ed46c1c31c",
+    "url": "./static/js/runtime-main.4d21a353.js"
+  }
+]);
